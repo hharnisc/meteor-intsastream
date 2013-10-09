93fa3864200eccfb9b1bc54c3cc9832c19bdef73
Harrison Harnisch <hharnisc@gmail.com>
2013-10-09 09:08:17 -0700

    use lighter instagram api

--- meteor-instastream/server/meteor-instastream-server.js
+++ meteor-instastream/server/meteor-instastream-server.js
@@ -1,26 +1,21 @@
 instaStream = new Meteor.Stream('insta');
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    var insta = Meteor.require('insta-stream');
-    var Instagram = new insta({client_id: 'client_id',
-                              client_secret: 'client_secret'});
-
-    Instagram.stream('popular', '', function(stream) {
-      stream.on('data', function(data) {
-        console.log('====> receiving ' + data.length + ' new posts');
-        _.each(_.first(data, 4), function(media) {
-          var caption = ''
-          if (media.caption) {
-            caption = media.caption.text
-          }
-          instaStream.emit('update', {caption: caption, 
-            image: media.images.standard_resolution.url,
-            likes: media.likes.count,
-            user: media.user.username
-          });
+  var instagram = Meteor.require('instagram').createClient('<client_id>', '<client_secret>');
+  Meteor.setInterval(function () {
+    instagram.media.popular(function (images, error) {
+      _.each(_.first(images, 4), function(media) {
+        var caption = '';
+        if (media.caption) {
+          caption = media.caption.text
+        }
+        instaStream.emit('update', {caption: caption, 
+          image: media.images.standard_resolution.url,
+          likes: media.likes.count,
+          user: media.user.username
         });
       });
     });
-  });
+  }, 3000);
+
 }
